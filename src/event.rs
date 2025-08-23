4aa4f52f4f9231ab96ce66f4d74d3e059df61b45
mow44 <mow44@proton.me>
2025-08-23 18:12:53 +0300

    Cleanup more

--- src/event.rs
+++ src/event.rs
@@ -21,8 +21,6 @@ pub enum Event {
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct EventHandler {
-    /// Event sender channel.
-    sender: mpsc::UnboundedSender<Event>,
     /// Event receiver channel.
     receiver: mpsc::UnboundedReceiver<Event>,
     /// Event handler thread.
@@ -34,7 +32,6 @@ impl EventHandler {
     pub fn new(tick_rate: u64) -> Self {
         let tick_rate = Duration::from_millis(tick_rate);
         let (sender, receiver) = mpsc::unbounded_channel();
-        let _sender = sender.clone();
         let handler = tokio::spawn(async move {
             let mut reader = crossterm::event::EventStream::new();
             let mut tick = tokio::time::interval(tick_rate);
@@ -42,24 +39,24 @@ impl EventHandler {
                 let tick_delay = tick.tick();
                 let crossterm_event = reader.next().fuse();
                 tokio::select! {
-                  _ = _sender.closed() => {
+                  _ = sender.closed() => {
                     break;
                   }
                   _ = tick_delay => {
-                    _sender.send(Event::Tick).unwrap();
+                    sender.send(Event::Tick).unwrap();
                   }
                   Some(Ok(evt)) = crossterm_event => {
                     match evt {
                       CrosstermEvent::Key(key) => {
                         if key.kind == crossterm::event::KeyEventKind::Press {
-                          _sender.send(Event::Key(key)).unwrap();
+                          sender.send(Event::Key(key)).unwrap();
                         }
                       },
                       CrosstermEvent::Mouse(mouse) => {
-                        _sender.send(Event::Mouse(mouse)).unwrap();
+                        sender.send(Event::Mouse(mouse)).unwrap();
                       },
                       CrosstermEvent::Resize(x, y) => {
-                        _sender.send(Event::Resize(x, y)).unwrap();
+                        sender.send(Event::Resize(x, y)).unwrap();
                       },
                       CrosstermEvent::FocusLost => {
                       },
@@ -72,11 +69,7 @@ impl EventHandler {
                 };
             }
         });
-        Self {
-            sender,
-            receiver,
-            handler,
-        }
+        Self { receiver, handler }
     }
 
     /// Receive the next event from the handler thread.
@@ -84,6 +77,6 @@ impl EventHandler {
     /// This function will always block the current thread if
     /// there is no data available and it's possible for more data to be sent.
     pub async fn next(&mut self) -> anyhow::Result<Event> {
-        self.receiver.recv().await.context("IO error")
+        self.receiver.recv().await.context("IO error") // TODO think about context
     }
 }
